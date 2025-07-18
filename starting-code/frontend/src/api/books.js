5fba2b32a69c11477cb6249f54dbec5649d6f46c
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-07-18 00:29:29 -0600

    Setup async function to update book

--- starting-code/frontend/src/api/books.js
+++ starting-code/frontend/src/api/books.js
@@ -28,5 +28,6 @@ export const getBooks = async () => {
 };
 
 // TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
+export const updateBook = async (id, newTitle, newStart, newEnd) => {};
 
 // TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
